065d8efc0c495e83c3a8551b307033528356acea
angel <angelabarreto431@gmail.com>
2025-01-05 20:31:55 -0300

    add operacao subtracao

--- modulo4/funcoes.go
+++ modulo4/funcoes.go
@@ -4,8 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println(soma(42, 13))
+
+	sub := subtracao(10, 5)
+	fmt.Println(sub)
 }
 
 func soma(x int, y int) int {
 	return x + y
+}
+
+func subtracao(x int, y int) int {
+	return x - y
 }
