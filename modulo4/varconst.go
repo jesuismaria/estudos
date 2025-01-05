587f591df9d26bb337d970b4edf3973008cc861b
angel <angelabarreto431@gmail.com>
2025-01-04 21:02:33 -0300

    Add varconst.go

---
+++ modulo4/varconst.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variáveis
+	// var + nome da variavel + tipo
+	var nome string
+	nome = "Bento"
+	fmt.Println(nome)
+
+	nome = "maria"
+	fmt.Println(nome)
+
+	var idade int
+	idade = 4
+	fmt.Println(idade)
+
+	var a = "maria"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b)
+	fmt.Println(c)
+
+	var d = true
+	fmt.Println(d)
+
+	f := "apple"
+	fmt.Println(f)
+
+	numero := 1
+	fmt.Println(numero)
+
+	//constantes
+	const idadeBento = 4
+	fmt.Println(idadeBento)
+}
