ee36e37b8cdf1c7b1d45760094261fa13f2fa472
angel <angelabarreto431@gmail.com>
2025-01-07 19:05:57 -0300

    add arquivo switch

---
+++ modulo4/switch.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	posicao := 2
+	switch posicap {
+	case 1:
+		fmt.Println("Primeiro lugar")
+	case 2:
+		fmt.Println("Segundo lugar")
+	case 3:
+		fmt.Println("Terceiro lugar")
+	}
+}
