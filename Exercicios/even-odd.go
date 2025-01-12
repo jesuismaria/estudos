08dc7aac03b00f4d3d0b3bd285f7972608960a1b
angel <angelabarreto431@gmail.com>
2025-01-12 09:55:02 -0300

    function returns even or odd

---
+++ Exercicios/even-odd.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 8
+	if num%2 == 0 {
+		fmt.Println("Even")
+	} else {
+		fmt.Println("Odd")
+	}
+}
+
+// Exercício "Even or Odd" do site Codewars
+// indicado no curso de Go da Comuniade Dev Completo
+// by Steph Cardoso
