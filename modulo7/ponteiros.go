0c579703f74196e977b380f29275d9dee8598b7b
angel <angelabarreto431@gmail.com>
2025-01-28 19:21:46 -0300

    add explicacao ponteiros

--- modulo7/ponteiros.go
+++ modulo7/ponteiros.go
@@ -5,6 +5,13 @@ import "fmt"
 // Ponteiros: uma variável que ao invés de armazenar
 // um valor (1, "olá", true ...) armazena uma memória
 
+func zeroValue(i int) {
+	i = 0
+}
+
+func zeroPointer(i *int) {
+	*i = 0
+}
 func main() {
 	//Memória -> essa memória tem um endereço -> endereço guarad valor
 
@@ -12,4 +19,21 @@ func main() {
 	fmt.Println("Valor inicial:", i)
 	fmt.Println("Valor end de memória:", &i)
 	//&variavel -> aponta para o endereço de memória da variável
+
+	zeroValue(i)
+	fmt.Println("zeroval:", i)
+
+	zeroPointer(&i)
+	fmt.Println("zeroptr:", i)
+
+	fmt.Println("pointer:", &i)
+
+	a := &i
+	fmt.Println("variável a:", a)
+	fmt.Println("Variável *a:", *a)
+	// usa o * em relação ao endereço! Não do valor
+	fmt.Println("variável &a:", &a)
 }
+
+// Aula 7.3 Ponteiros
+// by Steph Cardoso
