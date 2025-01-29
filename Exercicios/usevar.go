d2ecfbb226f35ec753f3106802d388be566dee2b
angel <angelabarreto431@gmail.com>
2025-01-29 19:33:58 -0300

    add exercicio atribuição com var

---
+++ Exercicios/usevar.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x, y, z)
+}
+
+// Exercício do curso de Go
+// da prof Ellen disponibilizado no youtube
